8f6fb9aded4fb0f76e2d7bf41bad57ac8307de3e
Tong Liu <tl543@login2.grace.ycrc.yale.edu>
2024-01-24 21:33:21 -0500

    Jan 2024 update

--- cucu_test.C
+++ cucu_test.C
@@ -1,7 +1,9 @@
 #include "hist_pp.h"
 void cucu_test(){
 
-TGraphErrors** result=hgpythia_ref();
-result[0]->Draw();
+//TGraphErrors** result=hgpythia_ref();
+//result[0]->Draw();
+TH1D** dau_hist=hist_dau();
+dau_hist[0]->Draw();
 
 }
